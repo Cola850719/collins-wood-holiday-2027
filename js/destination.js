3a3e0321d0dafe9cd1bd53561729f388c0a42124
Cola850719 <adamcollins0609@gmail.com>
2026-07-26 09:54:07 +1000

    Update destination.js

--- js/destination.js
+++ js/destination.js
@@ -1,1 +1,70 @@
+fetch("./data/destinations.json")
 
+.then(response => response.json())
+
+.then(destinations => {
+
+const container = document.getElementById("destination-list");
+
+
+destinations.forEach(destination => {
+
+
+const card = document.createElement("div");
+
+card.className = "card";
+
+
+card.innerHTML = `
+
+<h2>
+${destination.city}, ${destination.country}
+</h2>
+
+<p>
+💱 Currency: ${destination.currency}
+</p>
+
+<p>
+⭐ Family Score: ${destination.familyScore}/100
+</p>
+
+<p>
+🌦 October Weather:
+${destination.weatherOctober}
+</p>
+
+<p>
+💰 Cost Rating:
+${destination.costRating}/10
+</p>
+
+<p>
+👧 Kids Activities:
+${destination.kidsActivities}/10
+</p>
+
+<p>
+🏖 Beaches:
+${destination.beaches}/10
+</p>
+
+<p>
+🚤 Day Trips:
+${destination.dayTrips}/10
+</p>
+
+`;
+
+container.appendChild(card);
+
+
+});
+
+})
+
+.catch(error => {
+
+console.log("Error loading destinations:", error);
+
+});
