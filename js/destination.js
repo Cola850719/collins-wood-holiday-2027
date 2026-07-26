7ba58d53875af086876c1635ea68c8225bbc4c30
Cola850719 <adamcollins0609@gmail.com>
2026-07-26 17:00:58 +1000

    Restore destination cards after Feature 5 testing
    Refactor calculateFamilyScore function to improve readability and maintainability.

--- js/destination.js
+++ js/destination.js
@@ -37,9 +37,7 @@ fetch("/collins-wood-holiday-2027/data/destinations.json")
 
 function calculateFamilyScore(destination) {
 
-
     let score = 0;
-
 
     score += destination.kidsActivities * 2.5;
     score += destination.kidsClub * 2;
@@ -47,35 +45,19 @@ function calculateFamilyScore(destination) {
     score += destination.beaches * 1.5;
     score += destination.audValueRating * 1;
 
-
     if (destination.dailyCostAUD <= 150) {
-
         score += 5;
-
     } 
     else if (destination.dailyCostAUD <= 200) {
-
         score += 4;
-
     }
     else if (destination.dailyCostAUD <= 250) {
-
         score += 3;
-
     }
     else {
-
         score += 2;
-
     }
-
 
     return Math.round(score);
 
 }
-
-
-
-// Feature 5.1 - Ranking Reasons
-
-
