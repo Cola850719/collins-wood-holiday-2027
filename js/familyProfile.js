ab46fbc4c4df3384f43be3cfc251e14969db8020
Cola850719 <adamcollins0609@gmail.com>
2026-07-28 00:35:58 +1000

    Create familyProfile.js with travel information
    This file defines a family profile object with travel details and priorities.

---
+++ js/familyProfile.js
@@ -0,0 +1,36 @@
+console.log("Family Profile loaded");
+
+
+const familyProfile = {
+
+
+    travellers: "Family of 4",
+
+    departure: "Sydney, Australia",
+
+    travelMonth: "October 2027",
+
+    childAge: 7,
+
+
+    priorities: {
+
+        kidsActivities: 10,
+
+        kidsClub: 9,
+
+        beaches: 10,
+
+        poolResorts: 9,
+
+        valueForMoney: 9,
+
+        easyHoliday: 10
+
+    }
+
+
+};
+
+
+window.familyProfile = familyProfile;
